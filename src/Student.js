0f776277d0771c389da241747075c228299fd53b
anbashayan <anbashayan12@gmail.com>
2025-06-04 18:29:44 +0530

    mock commit

--- src/Student.js
+++ src/Student.js
@@ -2,7 +2,7 @@ function studentInfo(){
     const studentInfo = {props};
     const {name,indexNo} = studentInfo;
 }
-function studentInfo(){
-    const studentInfo = {props};
-    const {name,indexNo} = studentInfo;
+function staffInfo(){
+    const staffInfo = {props};
+    const {name,idNo} = staffInfo;
 }
