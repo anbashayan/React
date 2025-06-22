fb879813ac22ef17f2c47c9459f35eccccb0514a
anbashayan <anbashayan12@gmail.com>
2025-06-22 15:47:06 +0530

    header.module created for sperate css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,18 @@
+import './Header.css';
+import styles from './Header.module.css';
+
 function Header(){
 
     const myStyle = {
         color:"red",
-        backgroundColor:"lightblue"
+        backgroundColor:"lightblue",
+        padding:"10px",
+        fontFamily:"Arial"
     }
     return(
         <div>
-            <h1 style ={myStyle}>Hello Styling!!</h1>
+            {/* <h1 style ={myStyle}>Hello Styling!!</h1> */}
+            <h1 className ={styles.bigBlue}>Hello Styling!!</h1>
             <p>Add a little style</p>
         </div>
     )
