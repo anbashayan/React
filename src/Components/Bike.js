85884efa586ea3604d7c16a6388758b01f3d68cc
anbashayan <anbashayan12@gmail.com>
2025-06-25 23:59:25 +0530

    another state created

--- src/Components/Bike.js
+++ src/Components/Bike.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import styles from 'Head.module.css'
 
 function Bike(){
     const[color,setColor] = useState("Red");
     const[year,setYear] = useState("2018");
+    const[brand,setBrand] = useState("TVS");
+    const[model,setModel] = useState("Wego");
+
+    return(
+        <h1 className={StyleSheet.bigBlue}>Scooter Details</h1>
     )
 };
+export default Bike;
